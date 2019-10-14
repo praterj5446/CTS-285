c2e227985063486f878ed1323d8b36952684dc43
praterj5446 <47573930+praterj5446@users.noreply.github.com>
2019-10-14 12:41:01 -0400

    new

---
+++ Py test/Test/NumberGuesser-Test2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 12:23:28 2019
+
+@author: daniel
+"""
+import random
+def main():
+    hiddenNum = int()
+    guess = int()
+    again = "yes"
+    yesOrNo = ''
+    list = []
+    length = int()
+        
+    while again.lower() == "yes" or again.lower() == "y":
+        hiddenNum = random.randint(1,1000)
+        print("Guess The Number Between 1 And 1000")
+        print("With The Fewest Guesses!")
+        while hiddenNum != guess:
+            try:
+                guess = int(input("Enter a number: "))
+                if guess > hiddenNum:
+                    print("Too high. Try again.")
+                elif guess < hiddenNum:
+                    print("Too low. Try again.")
+                else:
+                    print("Congratulations. You guessed the number!",\
+                          hiddenNum)
+                list.append(guess)
+            except:
+                print("Please only integers!")
+        print(list)
+        length = len(list)
+        if length < 11:
+            print("Either you know the secret or you got lucky!")
+        elif length >= 11:
+            print("You should be able to do better!")
+        yesOrNo = input('Do you want to play again? ("Y/N"): ')
+        
+        while yesOrNo.lower() != "yes" and yesOrNo.lower() != "no" and \
+        yesOrNo.lower() != "y" and yesOrNo.lower() != "n":
+            yesOrNo = input('Do you want to play again? ("Y/N"): ')
+        if yesOrNo.lower() == "no" or yesOrNo.lower() == "n":
+            again = "no"
+            print("")
+        list.clear()
+
+main()
